ac91cb99625b335e97675bfe861ce1efcb8e7db3
Ben Lea <benjamin.lea@hotjar.com>
2021-03-06 03:31:51 +0000

    add some menus in a reusable component

---
+++ components/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div
+      class="w3-sidebar w3-bar-block w3-black w3-xxlarge"
+      style="width: 70px"
+    >
+      <NuxtLink to="/" class="w3-bar-item w3-button"
+        ><i class="fa fa-home"></i
+      ></NuxtLink>
+      <NuxtLink to="/booking" class="w3-bar-item w3-button"
+        ><i class="fa fa-search"></i
+      ></NuxtLink>
+
+      <a href="#" class="w3-bar-item w3-button"
+        ><i class="fa fa-envelope"></i
+      ></a>
+      <a href="#" class="w3-bar-item w3-button"><i class="fa fa-globe"></i></a>
+      <a href="#" class="w3-bar-item w3-button"><i class="fa fa-trash"></i></a>
+    </div>
+  </div>
+</template>
--- pages/booking.vue
+++ pages/booking.vue
@@ -4,25 +4,7 @@
     <div class="container">
       <div>
         <Logo />
-        <h1 class="title">therapistBookingApp</h1>
-        <div class="links">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="button--green"
-          >
-            Documentation
-          </a>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="button--grey"
-          >
-            GitHub
-          </a>
-        </div>
+        <h1 class="title">Booking</h1>
       </div>
     </div>
   </div>
