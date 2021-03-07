1e8e3786ed45fb751624b13ee4e24fd6287a0755
Ben Lea <benjamin.lea@hotjar.com>
2021-03-07 14:32:55 +0000

    add a calendly booking embed

--- pages/booking.vue
+++ pages/booking.vue
@@ -3,8 +3,19 @@
     <navbar />
     <div class="container">
       <div>
-        <Logo />
         <h1 class="title">Booking</h1>
+        <!-- Calendly inline widget begin -->
+        <div
+          class="calendly-inline-widget"
+          data-url="https://calendly.com/benjaminklea?hide_gdpr_banner=1"
+          style="min-width: 320px; height: 630px"
+        ></div>
+        <script
+          type="text/javascript"
+          src="https://assets.calendly.com/assets/external/widget.js"
+          async
+        ></script>
+        <!-- Calendly inline widget end -->
       </div>
     </div>
   </div>
